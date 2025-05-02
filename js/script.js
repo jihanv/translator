de7fc63c507196122b7502f2b870687fcd1ad262
Jihan <jihanv@gmail.com>
2025-05-02 22:57:38 +0900

    removed unused buttons

--- js/script.js
+++ js/script.js
@@ -1,17 +1,8 @@
 const fromText = document.querySelector(".from-text"),
 toText = document.querySelector(".to-text"),
-exchageIcon = document.querySelector(".exchange"),
-selectTag = document.querySelectorAll("select"),
 icons = document.querySelectorAll(".row i");
 translateBtn = document.querySelector("button"),
 
-selectTag.forEach((tag, id) => {
-    for (let country_code in countries) {
-        let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "hi-IN" ? "selected" : "";
-        let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
-        tag.insertAdjacentHTML("beforeend", option);
-    }
-});
 
 fromText.addEventListener("keyup", () => {
     if(!fromText.value) {
